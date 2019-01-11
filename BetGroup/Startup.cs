cb2bc7792f6b597078e9f0fb7cedb7bf732fdfd2
Filipe Trancoso <fhtrancoso@gmail.com>
2019-01-11 17:32:00 -0200

    Installing Swagger and creating projects libraries.

--- BetGroup/Startup.cs
+++ BetGroup/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BetGroup
 {
     public class Startup
     {
+        private const string SwaggerUrl = "/swagger/v1/swagger.json";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +29,23 @@ namespace BetGroup
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "API do BetGroup",
+                        Version = "v1",
+                        Description = "API do BetGroup",
+                        Contact = new Contact
+                        {
+                            Name = "Filipe Trancoso",
+                            Email = "fhtrancoso@gmail.com"
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +54,14 @@ namespace BetGroup
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Ativando middlewares para uso do Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint(SwaggerUrl,
+                        "API do BetGroup");
+                });
             }
             else
             {
